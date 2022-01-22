aca63cf6f48ec4a1cba337812f2585e461d1b17f
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-21 23:09:19 -0500

    Display de datos obtenidos desde API

--- src/App.js
+++ src/App.js
@@ -20,11 +20,39 @@ function App() {
     fetchJobs();
   }, []);
 
-  
+  if(loading) {
+    return(
+      <section className='section loading'>
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
 
+  const { company, dates, duties, title } = jobs[value];
   return (
-    <section className='section loading'>
-      <h1>Loading...</h1>
+    <section className='section'>
+      <div className='title'>
+        <h2>Experience</h2>
+        <div className='underline'></div>
+      </div>
+
+      <div className='jobs-center'>
+        <article className='job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {
+            duties.map((duty,key) => {
+              return (
+                <div key={key} className='job-desc'>
+                  <FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight> 
+                  <p>{duty}</p>
+                </div>
+              )
+            })
+          }
+        </article>
+      </div>
     </section>
   );
 }
