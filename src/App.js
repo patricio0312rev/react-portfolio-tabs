78b120dcb5f6a4c70c868a2c8f14173c324abeca
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-21 23:02:50 -0500

    Armand estructura inicial de App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa'
 
 const url = 'https://course-api.com/react-tabs-project';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+
+    setJobs(newJobs);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  
 
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <section className='section loading'>
+      <h1>Loading...</h1>
+    </section>
   );
 }
 
